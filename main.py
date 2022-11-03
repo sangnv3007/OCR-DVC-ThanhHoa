07a4c6183059d24817d45e759873dd4b98eebe9c
sangnv <sangnv@tandan.com.vn>
2022-11-03 15:24:58 +0700

    update 3/11/2022

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from fastapi import FastAPI, File, UploadFile
-from process import ReturnInfoCard, ExtractCard, MessageInfo
+from fastapi import FastAPI, File, UploadFile, Form
+from process import ReturnInfoCard
 import json
 import os
 import shutil
+import base64
 from starlette import status
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request
 from starlette.responses import Response
 from starlette.types import ASGIApp
+
 
 class LimitUploadSize(BaseHTTPMiddleware):
     def __init__(self, app: ASGIApp, max_upload_size: int) -> None:
@@ -22,28 +24,57 @@ class LimitUploadSize(BaseHTTPMiddleware):
             if content_length > self.max_upload_size:
                 return Response(status_code=status.HTTP_431_REQUEST_HEADER_FIELDS_TOO_LARGE)
         return await call_next(request)
+
+
 app = FastAPI()
-app.add_middleware(LimitUploadSize, max_upload_size=3000000)  # ~3MB
+app.add_middleware(LimitUploadSize, max_upload_size=4000000)  # ~3MB
+
+
 @app.post("/MembershipCard/upload")
 def upload(file: UploadFile = File(...)):
     try:
         pathSave = os.getcwd() + '/anhthe'
         if (os.path.exists(pathSave)):
-            with open(f'anhthe/{file.filename}','wb') as buffer:
+            with open(f'anhthe/{file.filename}', 'wb') as buffer:
                 shutil.copyfileobj(file.file, buffer)
         else:
             os.mkdir(pathSave)
-            with open(f'anhthe/{file.filename}','wb') as buffer:
+            with open(f'anhthe/{file.filename}', 'wb') as buffer:
                 shutil.copyfileobj(file.file, buffer)
         obj = ReturnInfoCard(f'anhthe/{file.filename}')
-        if(obj.errorCode==0):
-            return {"errorCode": obj.errorCode,"errorMessage": obj.errorMessage, 
-                    "data": [{"id": obj.id,"name": obj.name,"dob": obj.dob,"home": obj.home,"join_date":obj.join_date,
-                    "official_date": obj.official_date,"issued_by":obj.issued_by,"issue_date":obj.issue_date,"image":obj.image}]}
+        if (obj.errorCode == 0):
+            return {"errorCode": obj.errorCode, "errorMessage": obj.errorMessage,
+                    "data": [{"id": obj.id, "name": obj.name, "dob": obj.dob, "home": obj.home, "join_date": obj.join_date,
+                              "official_date": obj.official_date, "issued_by": obj.issued_by, "issue_date": obj.issue_date, "image": obj.image}]}
         else:
-            return {"errorCode": obj.errorCode,"errorMessage": obj.errorMessage, "data": []}
+            return {"errorCode": obj.errorCode, "errorMessage": obj.errorMessage, "data": []}
     except Exception:
         return {"message": "There was an error uploading the file"}
     finally:
         file.file.close()
     return {"message": f"Successfuly uploaded {file.filename}"}
+
+
+@app.post("/MembershipCard/uploadBase64")
+def uploadBase64(filename: str = Form(...), filedata: str = Form(...)):
+    try:
+        image_as_bytes = str.encode(filedata)  # convert string to bytes
+        img_recovered = base64.b64decode(image_as_bytes)  # decode base64string
+        pathSave = os.getcwd() +'\\'+ 'anhthe'
+        if (os.path.exists(pathSave)):
+            with open(f'anhthe\\{filename}', "wb") as f:
+                f.write(img_recovered)
+        else:
+            os.mkdir(pathSave)
+            with open(f'anhthe\\{filename}', "wb") as f:
+                f.write(img_recovered)
+        obj = ReturnInfoCard(f'anhthe\\{filename}')
+        if (obj.errorCode == 0):
+            return {"errorCode": obj.errorCode, "errorMessage": obj.errorMessage,
+                    "data": [{"id": obj.id, "name": obj.name, "dob": obj.dob, "home": obj.home, "join_date": obj.join_date,
+                              "official_date": obj.official_date, "issued_by": obj.issued_by, "issue_date": obj.issue_date, "image": obj.image}]}
+        else:
+            return {"errorCode": obj.errorCode, "errorMessage": obj.errorMessage, "data": []}
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    return {"message": f"Successfuly uploaded {filename}"}
--- process.py
+++ process.py
@@ -185,7 +185,7 @@ def ReturnInfoCard(path):
                 ), key=lambda item: item[1]): issued_by_text.append(i[0])
                 home_text = " ".join(home_text)
                 issued_by_text = " ".join(issued_by_text)
-                pathSave = os.getcwd() + '/dangvien/'
+                pathSave = os.getcwd() + '\\dangvien\\'
                 stringImage = "dangvien" + '_' + str(time.time()) + ".jpg"
                 if (os.path.exists(pathSave)):
                     cv2.imwrite(pathSave + stringImage, imgCrop)
@@ -222,8 +222,8 @@ class MessageInfo:
     def __init__(self, errorCode, errorMessage):
         self.errorCode = errorCode
         self.errorMessage = errorMessage
-obj = ReturnInfoCard('D:\TD.API_ML\Extracter-Membership-Card\imgTest\Membership (377).jpeg')
-print(obj.errorCode, obj.errorMessage)
+# obj = ReturnInfoCard('D:\TD.API_ML\Extracter-Membership-Card\imgTest\Membership (377).jpeg')
+# print(obj.errorCode, obj.errorMessage)
 #Crop anh 
 # path = 'D:\Download Chorme\Members\Detect_edge\obj'
 # i=199
