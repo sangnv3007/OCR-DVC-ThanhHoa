5a006abca846613eba255469c7bd24ff019fa622
sangnv <sangnv@tandan.com.vn>
2022-11-03 16:44:46 +0700

    Update new

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, File, UploadFile, Form
+from pydantic import BaseModel
+from typing import Union
 from process import ReturnInfoCard
 import json
 import os
@@ -9,8 +11,9 @@ from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoin
 from starlette.requests import Request
 from starlette.responses import Response
 from starlette.types import ASGIApp
-
-
+class Item(BaseModel):
+    name: str
+    stringbase64: Union[str, None] = None
 class LimitUploadSize(BaseHTTPMiddleware):
     def __init__(self, app: ASGIApp, max_upload_size: int) -> None:
         super().__init__(app)
@@ -29,9 +32,31 @@ class LimitUploadSize(BaseHTTPMiddleware):
 app = FastAPI()
 app.add_middleware(LimitUploadSize, max_upload_size=4000000)  # ~3MB
 
-
-@app.post("/MembershipCard/upload")
-def upload(file: UploadFile = File(...)):
+@app.post("/MembershipCard/uploadBase64")
+def uploadBase64(item: Item):
+    try:
+        image_as_bytes = str.encode(item.stringbase64)  # convert string to bytes
+        img_recovered = base64.b64decode(image_as_bytes)  # decode base64string
+        pathSave = os.getcwd() +'\\'+ 'anhthe'
+        if (os.path.exists(pathSave)):
+            with open(f'anhthe\\{item.name}', "wb") as f:
+                f.write(img_recovered)
+        else:
+            os.mkdir(pathSave)
+            with open(f'anhthe\\{item.name}', "wb") as f:
+                f.write(img_recovered)
+        obj = ReturnInfoCard(f'anhthe\\{item.name}')
+        if (obj.errorCode == 0):
+            return {"errorCode": obj.errorCode, "errorMessage": obj.errorMessage,
+                    "data": [{"id": obj.id, "name": obj.name, "dob": obj.dob, "home": obj.home, "join_date": obj.join_date,
+                              "official_date": obj.official_date, "issued_by": obj.issued_by, "issue_date": obj.issue_date, "image": obj.image}]}
+        else:
+            return {"errorCode": obj.errorCode, "errorMessage": obj.errorMessage, "data": []}
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    return {"message": f"Successfuly uploaded {item.name}"}
+@app.post("/MembershipCard/uploadFile")
+def uploadFile(file: UploadFile = File(...)):
     try:
         pathSave = os.getcwd() + '/anhthe'
         if (os.path.exists(pathSave)):
@@ -53,28 +78,3 @@ def upload(file: UploadFile = File(...)):
     finally:
         file.file.close()
     return {"message": f"Successfuly uploaded {file.filename}"}
-
-
-@app.post("/MembershipCard/uploadBase64")
-def uploadBase64(filename: str = Form(...), filedata: str = Form(...)):
-    try:
-        image_as_bytes = str.encode(filedata)  # convert string to bytes
-        img_recovered = base64.b64decode(image_as_bytes)  # decode base64string
-        pathSave = os.getcwd() +'\\'+ 'anhthe'
-        if (os.path.exists(pathSave)):
-            with open(f'anhthe\\{filename}', "wb") as f:
-                f.write(img_recovered)
-        else:
-            os.mkdir(pathSave)
-            with open(f'anhthe\\{filename}', "wb") as f:
-                f.write(img_recovered)
-        obj = ReturnInfoCard(f'anhthe\\{filename}')
-        if (obj.errorCode == 0):
-            return {"errorCode": obj.errorCode, "errorMessage": obj.errorMessage,
-                    "data": [{"id": obj.id, "name": obj.name, "dob": obj.dob, "home": obj.home, "join_date": obj.join_date,
-                              "official_date": obj.official_date, "issued_by": obj.issued_by, "issue_date": obj.issue_date, "image": obj.image}]}
-        else:
-            return {"errorCode": obj.errorCode, "errorMessage": obj.errorMessage, "data": []}
-    except Exception:
-        return {"message": "There was an error uploading the file"}
-    return {"message": f"Successfuly uploaded {filename}"}
